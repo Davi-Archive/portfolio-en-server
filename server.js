79a4c46ce503abb819de843684837d0dfac31d37
davi38 <davi4alves@gmail.com>
2022-09-27 18:03:44 -0300

    feat: backend start

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require("dotenv").config({ path: "./.env" });
+const colors = require('colors');
+const connectDB = require('./config/db.js')
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/',(req,res)=>{
+    res.status(200).json({msg: 'working!'})
+})
+
+
+const PORT = process.env.PORT || 3001;
+connectDB()
+
+app.listen(PORT, ()=>{
+    console.log(`Server Running on port ${PORT}`)
+})
