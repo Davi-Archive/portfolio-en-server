b46b74d0a1f75cc93de736239d7d74237c2c9833
davi38 <davi4alves@gmail.com>
2022-09-27 19:57:32 -0300

    feat: cors for cross origin access

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require("dotenv").config({ path: "./.env" });
 const colors = require('colors');
+var cors = require("cors");
+
 const connectDB = require('./config/db.js')
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
